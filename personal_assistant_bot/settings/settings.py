442d38d330881d3c051f4e58966c8d13152ff057
Avtarso <t0676352927@gmail.com>
2024-03-12 20:16:23 +0200

    fix work database in container

--- personal_assistant_bot/settings/settings.py
+++ personal_assistant_bot/settings/settings.py
@@ -1,7 +1,10 @@
 from pathlib import Path
 
-addressbook_filename = Path(Path.home(), "adressbook.pkl")
-notes_filename = Path(Path.home(), "notes.pkl")
+# addressbook_filename = Path(Path.home(), "adressbook.pkl")
+# notes_filename = Path(Path.home(), "notes.pkl")
+
+addressbook_filename = "adressbook.pkl"
+notes_filename = "notes.pkl"
 
 PAG = 4
 NOTE_LEN = 40
